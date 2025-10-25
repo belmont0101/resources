551fdd30e6936f6224b632475772a69809693fc0
Belmont <MaxGamerDK09@gmail.com>
2025-10-25 22:23:25 +0200

    first commit

---
+++ [belmont]/bel_newscript/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "bel_newscript"
+description "Test of new scripts"
+author "Belmont0101"
+version "0.0.1"
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
